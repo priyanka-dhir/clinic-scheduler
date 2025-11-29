dc52eba40d743e28074e8fa424b6bdf97ce5dd9b
priyanka-dhir <priyankadhir.mehta@yahoo.com>
2025-11-28 21:50:34 -0500

    replaced the local host url to use back end render LIVE url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001', // backend URL
+  baseURL: process.env.REACT_APP_BACKEND_URL, // Use environment variable
 });
 
 API.interceptors.request.use((config) => {
